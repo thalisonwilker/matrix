70178f01fae95fcd5b754d1f5ac14791e42dfa6e
Thalyson Wilker <thalison.wilker@gmail.com>
2021-12-21 14:11:01 -0300

    update to python3

--- matrix.py
+++ matrix.py
@@ -1,15 +1,22 @@
-import sys,time
+#! /usr/bin/python3
+import time
+import os
+os.system('clear')
+frase = ['Wake up, Neo...','The matrix has you...','Follow the white rabbit...']
+for i in frase:
+    for c in i:
+        if( c == '.' ):
+            time.sleep(0.3)
+        elif ( c == ' ' ):
+            time.sleep(0.4)
+        else:
+            time.sleep(0.2)
+        print(c, sep='', end='', flush=True)
+    print(sep="\n")
+    time.sleep(0.7)
+    os.system('clear')
 
-def matrix():
-    frase = ['Wake up, Neo','The matrix has you','Follow the white rabbit...']
-    
-    for i in frase:
-        for c in i:
-            sys.stdout.write(c)
-            time.sleep(0.12)
-        time.sleep(1)
-        print ''
-    time.sleep(2)
-    print 'Knock, Kcnock, Neo.'
+time.sleep(3)
+print('Knock, Kcnock, Neo.')
+time.sleep(30)
 
-matrix()
